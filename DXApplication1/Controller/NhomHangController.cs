570560124a0578198310722ac6d2ed7afbcf483b
Tiennguyen25081996 <tiennguyen25081996@gmail.com>
2017-05-30 17:55:34 +0700

    tiến sửa bài quản lý bán hàng

---
+++ DXApplication1/Controller/NhomHangController.cs
@@ -0,0 +1,17 @@
+﻿using DXApplication1.DAL;
+using DXApplication1.Model;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace DXApplication1.Controller
+{
+   public class NhomHangController : _ControllerBase<NHOMHANG>
+    {
+
+    }
+}
---
+++ DXApplication1/Model/KHO.cs
@@ -0,0 +1,29 @@
+﻿using DXApplication1.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DXApplication1.Model
+{
+    public class KHO: _ModelBase
+    {
+        public string MaKho { get { return GetSTR(0); } set { SetData(0, value); } }
+        public string TenKho { get { return GetSTR(1); } set { SetData(1, value); } }
+        public string ViTri { get { return GetSTR(2); } set { SetData(2, value); } }
+        public string MaNV { get { return GetSTR(3); } set { SetData(3, value); } }
+
+
+        public override void Setup()
+        {
+            MaxFields = 4;
+            ValueData = new object[MaxFields];
+        }
+
+        public override Type GetDataType()
+        {
+            return this.GetType();
+        }
+    }
+}
